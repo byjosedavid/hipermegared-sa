82104f3e4fcaa2faf77668cf619da1133f3bbc04
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-06-07 11:53:26 -0500

    form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoComponent } from './info/info.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InfoComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
